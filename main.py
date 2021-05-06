a01b74d72425af5a8b0f7db4119fd7a0cc7c4810
multipassport <mashross@gmail.com>
2021-05-06 13:31:17 +0300

    Define function to thumbmail images to set size

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import instabot
 import os
 import requests
 import urllib3
 
-
 from pathlib import Path
+from PIL import Image
+from requests.exceptions import ConnectionError
 from urllib.parse import urlsplit, unquote, urlunparse
 
 
@@ -35,12 +37,20 @@ def get_file_extension(url):
 
 def fetch_hubble_photo(url, image_id, filepath, extension):
     hubble_image_filepath = os.path.join(filepath, f'{image_id}{extension}')
-    print(url)
     response = requests.get(url, verify=False)
     response.raise_for_status()
 
     with open(hubble_image_filepath, 'wb') as file:
         file.write(response.content)
+
+
+def thumbnail_image(photo, image_size):
+    original_image = Image.open(photo)
+    original_image.thumbnail(image_size)
+    ready_to_publish_image = original_image.convert('CMYK')
+    new_image_name = photo.replace(get_file_extension(photo), '.jpg')
+    ready_to_publish_image.save(new_image_name, format='JPEG')
+    return ready_to_publish_image
 
 
 if __name__ == '__main__':
@@ -56,15 +66,30 @@ if __name__ == '__main__':
 
     #     fetch_spacex_launch(link, filepath)
 
+    # hubble_collection = 'holiday_cards'
+    # hubble_collection_url = f'http://hubblesite.org/api/v3/images/{hubble_collection}'
+    # response = requests.get(hubble_collection_url)
+    # response.raise_for_status()
+    # collection_image_ids = [image_detail['id'] for image_detail in response.json()]
 
-    image_id = 1
-    hubble_url = f'http://hubblesite.org/api/v3/image/{image_id}'
+    # for image_id in collection_image_ids:
+    #     try:
+    #         hubble_url = f'http://hubblesite.org/api/v3/image/{image_id}'
+    #         response = requests.get(hubble_url)
+    #         response.raise_for_status()
+    #     except ConnectionError:
+    #         print('Error')
+    #     else:
+    #         hubble_image_link = f'http:{response.json()["image_files"][-1]["file_url"]}'
+    #         print('downloading', image_id)
 
-    response = requests.get(hubble_url)
-    response.raise_for_status()
-    image_links = response.json()['image_files']
-    hubble_image_links = [f'http:{link["file_url"]}' for link in response.json()['image_files']]
-
-    # pprint.pprint(hubble_image_links)
-    fetch_hubble_photo(hubble_image_links[-1], image_id, folder, get_file_extension(hubble_image_links[-1]))
+    #         fetch_hubble_photo(hubble_image_link, image_id, folder, get_file_extension(hubble_image_link))
     
+    # image_size = (1200, 1200)
+    # print(thumbnail_image('./images/4782.png', image_size))
+
+
+    
+
+
+
