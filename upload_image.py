bbd367bf143319d49e9344a3fa6dd1a637a0ad7d
multipassport <mashross@gmail.com>
2021-05-08 13:45:14 +0300

    Create script uploading images to instagram

---
+++ upload_image.py
@@ -0,0 +1,43 @@
+import glob
+import os
+
+from dotenv import load_dotenv
+from instabot import Bot
+from PIL import Image
+from urllib.parse import urlsplit
+
+
+def thumbnail_image(photo, image_size):
+    new_image_name = photo.replace(os.path.splitext(photo)[-1], '.jpg')
+    image = Image.open(photo)
+    image.thumbnail(image_size)
+    image.convert('RGB').save(new_image_name, 'JPEG')
+    return image
+
+
+def get_file_extension(url):
+    extension = os.path.splitext(urlsplit(url).path)[-1]
+    return extension
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    logging.basicConfig(filename="upload_image.log", filemode='w')
+
+    folder_path = "./images"
+    image_maximal_size = (1080, 1080)
+    images = [image for image in glob.glob(folder_path + '/*.jpg')]
+
+    for image in os.listdir('./images'):
+        thumbnail_image(f'./images/{image}', image_maximal_size)
+
+    bot = Bot()
+    bot.login(username=os.getenv('INSTAGRAM_USERNAME'), password=os.getenv('PASSWORD'))
+
+    for image in images:
+        try:
+            print(f'Uploading {image}')
+            bot.upload_photo(image)
+        except Exception as error:
+            print(str(error))
+            logging.error(error)
