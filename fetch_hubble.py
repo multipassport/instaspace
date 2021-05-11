2c7763ffe47ad5d2f5011bbc36849bd81e567024
multipassport <mashross@gmail.com>
2021-05-11 21:06:47 +0300

    Shorten if __name__ construction by splitting it to separate functions

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -6,8 +6,6 @@ from pathlib import Path
 from requests.exceptions import ConnectionError
 from urllib.parse import urlsplit
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 
 def get_file_extension(url):
     extension = os.path.splitext(urlsplit(url).path)[-1]
@@ -18,32 +16,39 @@ def fetch_hubble_photo(url, image_id, filepath, extension):
     hubble_image_filepath = os.path.join(filepath, f'{image_id}{extension}')
     response = requests.get(url, verify=False)
     response.raise_for_status()
-
     with open(hubble_image_filepath, 'wb') as file:
         file.write(response.content)
 
 
+def fetch_hubble_collection_image_ids(collection_name):
+    hubble_collection_url = f'http://hubblesite.org/api/v3/images/{collection_name}'
+    response = requests.get(hubble_collection_url)
+    response.raise_for_status()
+    collection_image_ids = [image_detail['id'] for image_detail in response.json()]
+    return collection_image_ids
+
+
+def get_image_link(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    hubble_image_link = f'http:{response.json()["image_files"][-1]["file_url"]}'
+    return hubble_image_link
+            
+
 if __name__ == '__main__':
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    
     folder = './images'
     Path(folder).mkdir(exist_ok=True)
 
     hubble_collection = 'holiday_cards'
-    hubble_collection_url = f'http://hubblesite.org/api/v3/images/{hubble_collection}'
-
-    response = requests.get(hubble_collection_url)
-    response.raise_for_status()
-    collection_image_ids = [image_detail['id'] for image_detail in response.json()]
+    collection_image_ids = fetch_hubble_collection_image_ids(hubble_collection)
 
     for image_id in collection_image_ids:
         try:
             hubble_url = f'http://hubblesite.org/api/v3/image/{image_id}'
-            response = requests.get(hubble_url)
-            response.raise_for_status()
+            hubble_image_link = get_image_link(hubble_url)
+            extension = get_file_extension(hubble_image_link)
+            fetch_hubble_photo(hubble_image_link, image_id, folder, extension)
         except ConnectionError:
             print('Connection Error')
-        else:
-            hubble_image_link = f'http:{response.json()["image_files"][-1]["file_url"]}'
-            extension = get_file_extension(hubble_image_link)
-
-            print('downloading', image_id)
-            fetch_hubble_photo(hubble_image_link, image_id, folder, extension)
